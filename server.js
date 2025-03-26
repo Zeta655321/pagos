2fe361410ad3a35b27e5bd9d64f59d7f2ca0fb6c
Zeta655321 <103940585+Zeta655321@users.noreply.github.com>
2025-03-26 18:02:20 -0300

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Verificar si el ACCESS_TOKEN está presente
+if (!process.env.ACCESS_TOKEN) {
+    console.error("ACCESS_TOKEN de Mercado Pago no configurado.");
+    process.exit(1); // Salir del servidor si no está configurado el token
+}
+
 // Configurar Mercado Pago correctamente
 const client = new mercadopago.MercadoPagoConfig({
     accessToken: process.env.ACCESS_TOKEN
@@ -14,10 +20,6 @@ const client = new mercadopago.MercadoPagoConfig({
 // Middleware para parsear JSON y habilitar CORS
 app.use(express.json());
 app.use(cors());
-
-
-
-
 
 // Ruta para crear una preferencia de pago
 app.post("/crear-preferencia", async (req, res) => {
